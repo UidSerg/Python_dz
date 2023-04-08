d8ebf674c51f46980c25210ce0baca793fbfacb6
Uidserg <uidserg@live.com>
2023-04-08 21:10:29 +0500

    1-ок

---
+++ zadanie_1.py
@@ -0,0 +1,7 @@
+print("Введите 3х значное число:")
+value = int(input())
+result = 0
+while value / 10 > 0:
+    result += value % 10
+    value = int(value / 10)
+print(f"Cумму цифр трехзначного числа = {result} ")
